d0518ec9a721c9b8dadd76ad782ddd9623505d19
Linajja <94914937+linajja@users.noreply.github.com>
2022-02-16 22:01:27 +0200

    changes

--- main.js
+++ main.js
@@ -1,32 +1,61 @@
 
-function createCard() {
-    let img = document.createElement("img");
-    img.className = "img";
-    img.src = "https://randomuser.me/api/portraits/med/women/20.jpg";
+// function createCard(niceImg, nameSurname, numberAge, fullAdress, fullEmail) {
+//     let newImg = document.createElement("img");
+//     newImg.className = "img";
+//     newImg.src = niceImg;
 
-    let name = document.createElement("name");
-    name.textContent = "Loreta Loretaite";
+//     let newName = document.createElement("name");
+//     newName.textContent = nameSurname;
 
-    let age = document.createElement("age");
-    age.textContent = "35 age";
+//     let newAge = document.createElement("age");
+//     newAge.textContent = numberAge;
 
-    let adress = document.createElement("adress");
-    adress.textContent = "Lauku gatve 4";
+//     let newAdress = document.createElement("adress");
+//     newAdress.textContent = fullAdress;
 
-    let email = document.createElement("email");
-    email.textContent = "lor.lor@gmail.com";
+//     let newEmail = document.createElement("email");
+//     newEmail.textContent = fullEmail;
 
-    let newCard = document.createElement("div");
-    newCard.className = "item";
+//     let newCard = document.createElement("div");
+//     newCard.className = "item";
 
-    newCard.append(img, name, age, adress, email);
+//     newCard.append(niceImg, nameSurname, numberAge, fullAdress, fullEmail);
 
-    return newCard;
-};
+//     return newCard;
+// };
 
-let parent = document.getElementById("main");
-let card = createCard();
-parent.appendChild(card);
+// let img1 = "https://i.pinimg.com/564x/44/15/ba/4415ba5df0f4bfcee5893d6c441577e0.jpg";
+// let name1 = "Jonas Jonaitis";
+// let age1 = "35 age";
+// let adress1 = "Ateities takas 5";
+// let email1 = "jonas.junukas@gmail.com";
 
+// let parent = document.getElementById("main");
+// let card = createCard(img1, name1, age1, adress1, email1);
+// parent.append(card);
 
+// function appendCard() {
 
+// }
+
+async function getData() {
+    const requestURL = 'https://randomuser.me/api/';
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const data = await response.json();
+
+    console.log(data);
+
+    let { email, location, name } = data.results[0];
+    let email = '${email}';
+    console.log(email);
+    let location = '${location.city}' '${location.street.name}';
+    console.log(location);
+    let name = '${name.first}' '${name.last}';
+    console.log(name);
+    // console.log(data.results[0].email);
+    // console.log(data.results[0].location.city);
+    // console.log(data.results[0].location.street.name);
+    // let { first, last } = data.results[0].name;
+    // console.log(first + " " + last);
+}
