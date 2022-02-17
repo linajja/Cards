0c1b4ac2c014cb8151157134346a36454b3d63b6
Linajja <94914937+linajja@users.noreply.github.com>
2022-02-17 19:07:02 +0200

    js

--- main.js
+++ main.js
@@ -19,39 +19,37 @@ function createCard(niceImg, nameSurname, numberAge, fullAdress, fullEmail) {
     let newCard = document.createElement("div");
     newCard.className = "item";
 
-    newCard.append(newImg, newName, newAge, newEmail);
+    newCard.append(newImg, newName, newAge, newAdress, newEmail);
 
     return newCard;
 };
 
+function appendCard(card) {
+    let parent = document.getElementById("main");
+    parent.append(card);
+}
 async function getData() {
     const requestURL = 'https://randomuser.me/api/';
     const request = new Request(requestURL);
-    const response = await fetch(request);
+    const response = await fetch(request, { mode: 'cors' });
     const data = await response.json();
+    console.log(data);
 
-    let { first, last } = data.results[0].name;
-    let { email } = data.results[0];
-    let { name, number } = data.results[0].location.street;
-    let { age } = data.results[0].dob;
-    let { medium } = data.results[0].picture;
-    let allInfo = `${medium}, ${first} ${last}, ${age}, ${name} ${number}, ${email}`
-    return allInfo;
-}
-
-function appendCard() {
-
-    //     // let img1 = "https://i.pinimg.com/564x/44/15/ba/4415ba5df0f4bfcee5893d6c441577e0.jpg";
-    //     // let name1 = "Jonana Jona";
-    //     // let age1 = "35 age";
-    //     // let adress1 = "Ateities takas 5";
-    //     // let email1 = "jonas.junukas@gmail.com";
-
-    let parent = document.getElementById("main");
-    let card = createCard(getData(0), getData(1), getData(2), getData(3), getData(4));
-    console.log(card);
-    parent.append(card);
+    return data;
 }
 
 let btn = document.getElementById("btn");
-btn.addEventListener("click", appendCard);
+btn.addEventListener("click", function () {
+    getData().then(data => {
+        let { picture, name, dob, location, email } = data.results[0];
+        picture = `${picture.large}`;
+        name = `${name.first} ${name.last}`;
+        dob = dob.age;
+        location = `${location.country}, ${location.city}`;
+        email = email;
+        let card = createCard(picture, name, dob, location, email);
+        appendCard(card);
+    }).catch(error => {
+        console.log(error);
+    });
+})
