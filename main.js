62985662e53be4c2908eb4b9c71cbfd8c28984c3
linajja <lina@netmarkuab.com>
2022-02-17 16:44:54 +0200

    changes

--- main.js
+++ main.js
@@ -1,42 +1,28 @@
 
-// function createCard(niceImg, nameSurname, numberAge, fullAdress, fullEmail) {
-//     let newImg = document.createElement("img");
-//     newImg.className = "img";
-//     newImg.src = niceImg;
+function createCard(niceImg, nameSurname, numberAge, fullAdress, fullEmail) {
+    let newImg = document.createElement("img");
+    newImg.className = "img";
+    newImg.src = niceImg;
 
-//     let newName = document.createElement("name");
-//     newName.textContent = nameSurname;
+    let newName = document.createElement("name");
+    newName.textContent = nameSurname;
 
-//     let newAge = document.createElement("age");
-//     newAge.textContent = numberAge;
+    let newAge = document.createElement("age");
+    newAge.textContent = numberAge;
 
-//     let newAdress = document.createElement("adress");
-//     newAdress.textContent = fullAdress;
+    let newAdress = document.createElement("adress");
+    newAdress.textContent = fullAdress;
 
-//     let newEmail = document.createElement("email");
-//     newEmail.textContent = fullEmail;
+    let newEmail = document.createElement("email");
+    newEmail.textContent = fullEmail;
 
-//     let newCard = document.createElement("div");
-//     newCard.className = "item";
+    let newCard = document.createElement("div");
+    newCard.className = "item";
 
-//     newCard.append(niceImg, nameSurname, numberAge, fullAdress, fullEmail);
+    newCard.append(newImg, newName, newAge, newEmail);
 
-//     return newCard;
-// };
-
-// let img1 = "https://i.pinimg.com/564x/44/15/ba/4415ba5df0f4bfcee5893d6c441577e0.jpg";
-// let name1 = "Jonas Jonaitis";
-// let age1 = "35 age";
-// let adress1 = "Ateities takas 5";
-// let email1 = "jonas.junukas@gmail.com";
-
-// let parent = document.getElementById("main");
-// let card = createCard(img1, name1, age1, adress1, email1);
-// parent.append(card);
-
-// function appendCard() {
-
-// }
+    return newCard;
+};
 
 async function getData() {
     const requestURL = 'https://randomuser.me/api/';
@@ -44,18 +30,28 @@ async function getData() {
     const response = await fetch(request);
     const data = await response.json();
 
-    console.log(data);
-
-    let { email, location, name } = data.results[0];
-    let email = '${email}';
-    console.log(email);
-    let location = '${location.city}' '${location.street.name}';
-    console.log(location);
-    let name = '${name.first}' '${name.last}';
-    console.log(name);
-    // console.log(data.results[0].email);
-    // console.log(data.results[0].location.city);
-    // console.log(data.results[0].location.street.name);
-    // let { first, last } = data.results[0].name;
-    // console.log(first + " " + last);
+    let { first, last } = data.results[0].name;
+    let { email } = data.results[0];
+    let { name, number } = data.results[0].location.street;
+    let { age } = data.results[0].dob;
+    let { medium } = data.results[0].picture;
+    let allInfo = `${medium}, ${first} ${last}, ${age}, ${name} ${number}, ${email}`
+    return allInfo;
 }
+
+function appendCard() {
+
+    //     // let img1 = "https://i.pinimg.com/564x/44/15/ba/4415ba5df0f4bfcee5893d6c441577e0.jpg";
+    //     // let name1 = "Jonana Jona";
+    //     // let age1 = "35 age";
+    //     // let adress1 = "Ateities takas 5";
+    //     // let email1 = "jonas.junukas@gmail.com";
+
+    let parent = document.getElementById("main");
+    let card = createCard(getData(0), getData(1), getData(2), getData(3), getData(4));
+    console.log(card);
+    parent.append(card);
+}
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", appendCard);
